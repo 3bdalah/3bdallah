ecd0e51b19704791edbe22003a903027e74f720b
3bdalah <3bdallah.dev@gmail.com>
2021-08-04 11:06:02 +0200

    edited userneam twitter

--- components/Footer.js
+++ components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
                 </div>
                 <div className="list-socail">
                      <ul className="list">
-                       <li><a href="https://twitter.com/3bdallahdev" target="_blank"><FiTwitter/></a></li>
-                       <li><a href="https://github.com/3bdalah" target="_blank"><FiGithub/></a></li>
-                       <li><a href="https://www.linkedin.com/in/abdallah-abdelghaffar-65136614b/" target="_blank"><FiLinkedin/></a></li>
+                       <li><a href="https://twitter.com/3bdalIah_" target="_blank" rel="noreferrer"><FiTwitter/></a></li>
+                       <li><a href="https://github.com/3bdalah" target="_blank" rel="noreferrer"><FiGithub/></a></li>
+                       <li><a href="https://www.linkedin.com/in/abdallah-abdelghaffar-65136614b/" rel="noreferrer"  target="_blank"><FiLinkedin/></a></li>
                     </ul>
                 </div>
                 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// const withMDX = require('@next/mdx')({
-//   extension: /\.mdx$/
-// })
-module.exports = {
-  // pageExtensions: ['js', 'jsx', 'mdx'],
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
-};
+});
