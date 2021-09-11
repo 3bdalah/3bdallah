8252f8dc9b1db4eb3342478cd1f3a3b4b1c1687f
Abdallah <3bdallah.dev@gmail.com>
2021-09-11 20:42:36 +0200

    Update Footer.js
    change username twitter

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 </div>
                 <div className="list-socail">
                      <ul className="list">
-                       <li><a href="https://twitter.com/3bdalIah_" target="_blank" rel="noreferrer"><FiTwitter/></a></li>
+                       <li><a href="https://twitter.com/_3abdalah_" target="_blank" rel="noreferrer"><FiTwitter/></a></li>
                        <li><a href="https://github.com/3bdalah" target="_blank" rel="noreferrer"><FiGithub/></a></li>
                        <li><a href="https://www.linkedin.com/in/abdallah-abdelghaffar-65136614b/" rel="noreferrer"  target="_blank"><FiLinkedin/></a></li>
                     </ul>
