f50e01337fc8e27855ce56cfced5e929546bbe0f
Abdallah <3bdallah.dev@gmail.com>
2021-12-15 00:17:56 +0200

    delete (Links) : remove element from links
    delete link blog until finish this section and fix some bugs

--- components/Links.js
+++ components/Links.js
@@ -4,11 +4,10 @@ const Links = () => {
         
             <ul className="list_links">
                    <li className='link'><Link href="/"><a>About</a></Link></li>
-                    <li className='link'><Link href="/blog"><a>Blog</a></Link></li>
+                    {/* <li className='link'><Link href="/blog"><a>Blog</a></Link></li>*/}
                     <li className='link'><Link href="/projects"><a>Works</a></Link></li>
                     {/* <li className='link'><Link href="/experince"><a>exprince</a></Link></li>   */}
             </ul>
-        
     );
 }
 export default Links;
