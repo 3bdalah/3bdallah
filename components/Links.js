accf3d642a1c3ac71fa4c40db2d186246cb6d46d
Abdallah <3bdallah.dev@gmail.com>
2021-12-17 13:40:46 +0200

    update [links] : remove link resume

--- components/Links.js
+++ components/Links.js
@@ -6,7 +6,7 @@ const Links = () => {
                    <li className='link'><Link href="/"><a>About</a></Link></li>
                     {/* <li className='link'><Link href="/blog"><a>Blog</a></Link></li>*/}
                     <li className='link'><Link href="/projects"><a>Works</a></Link></li>
-                    <li className='link'><Link href="https://github.com/3bdalah/my-resum/raw/master/Abdallah%20CV.pdf"><a>Resume</a></Link></li>
+                    {/*  <li className='link'><Link href="https://github.com/3bdalah/my-resum/raw/master/Abdallah%20CV.pdf"><a>Resume</a></Link></li>*/}
                     {/* <li className='link'><Link href="/experince"><a>exprince</a></Link></li>   */}
             </ul>
     );
