41b6f611bd6a0a045d2de8faadf9875df3d1bc14
Abdallah <3bdallah.dev@gmail.com>
2023-06-17 03:56:55 +0300

    Update : Projects.js :
    - add new project [Egy best] Movies

--- data/Projects.js
+++ data/Projects.js
@@ -1,48 +1,57 @@
 const projects_data = [
     {
         id: 1,
-        title: "zara store",
+        title: "Zara store",
         img : "./img-public/project2.jpg",
-        skills: ['css','react','bootstrap','redux'],
-        desc:"E-Commerse is the buying and selling of goods and services,online store clothes,App have single page application and add products to cart and another features",
-        demo:"https://zarastore.netlify.app/",
-        git:"https://github.com/3bdalah/zara"
+        skills: ['css', 'react', 'bootstrap', 'redux'],
+        desc: "E-Commerce is the buying and selling of goods and services, online store clothes, App have single page application and add products to cart and another feature",
+        demo: "https://zarastore.netlify.app/",
+        git: "https://github.com/3bdalah/zara"
     },
     {
         id: 2,
         title: "Academy",
         img : "./img-public/project3.jpg",
-        skills: ['css','react','bootstrap','hooks'],
-        desc:"academy online and offline and i used Hooks Usestate and useContext and Created Api courses",
-        demo:"https://megira.netlify.app/",
-        git:"https://github.com/3bdalah/academy"
+        skills: ['css', 'react', 'bootstrap', 'hooks'],
+        desc: "academy online and offline and I used Hooks Usestate and use context and Created Api courses",
+        demo: "https://megira.netlify.app/",
+        git: "https://github.com/3bdalah/academy"
     },
     {
         id: 3,
-        title: "Blog",
+        title: "Egy-Best",
         img : "./img-public/project4.jpg",
-        skills: ['css','NextJs','react'],
-        desc:"just simple blog and use fetch api and display data post on page posts",
-        demo:"https://3bdallahdev.netlify.app/",
-        git:"https://github.com/3bdalah/Blog-nextJs"
+        skills: ['Css', 'Html', 'Javascript'],
+        desc: "The project website allows users to browse, stream, and search for movies, providing a comprehensive platform for movie enthusiasts.",
+        demo: "https://egy-best-3bdallah.vercel.app/",
+        git: "https://github.com/3bdalah/Egy-best"
     },
     {
         id: 4,
-        title: "static website",
+        title: "Static website",
         img : "./img-public/project5.jpg",
-        skills: ['html','css','bootstrap','responsive'],
-        desc:"landing Page just practice",
-        demo:"https://reloadacademy1.netlify.app/",
-        git:"https://github.com/3bdalah/landing-page"
+        skills: ['HTML','css', 'bootstrap', 'responsive'],
+        desc: "landing Page just practice",
+        demo: "https://reloadacademy1.netlify.app/",
+        git: "https://github.com/3bdalah/landing-page"
     },
     {
         id: 5,
         title: "Far Away",
         img : "./img-public/project2.jpg",
-        skills: ['css','React'],
-        desc:"todo list add item and delete and packed item ",
-        demo:"https://first-app-3bdallah.vercel.app/",
-        git:"https://github.com/3bdalah/first-app"
+        skills: ['css',' React'],
+        desc: "todo list add an item and delete and packed item ",
+        demo: "https://first-app-3bdallah.vercel.app/",
+        git: "https://github.com/3bdalah/first-app"
     },
+    {
+        id: 6,
+        title: "Blog",
+        img : "./img-public/project4.jpg",
+        skills: ['css', 'NextJs', 'react'],
+        desc: "Just simple blog and use fetch API and display data post on page posts",
+        demo: "https://3bdallahdev.netlify.app/",
+        git: "https://github.com/3bdalah/Blog-nextJs"
+    }
 ];
 export default projects_data;
